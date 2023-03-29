c0fd6fcaaba524d16a12be153dd6c4bf0d578751
Tan Gia Luong <tangia123@outlook.com.vn>
2023-03-29 13:19:08 +0700

    Fix Cart_Client

--- client_/src/components/mainpages/cart/Cart.js
+++ client_/src/components/mainpages/cart/Cart.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalState } from "../../../GlobalState";
+import { Link } from "react-router-dom";
 
 function Cart() {
-    return <div>Cart component</div>;
+    const state = useContext(GlobalState);
+    const [cart, setCart] = state.userAPI.cart;
+
+    if (cart.length === 0)
+        return (
+            <h2 style={{ textAlign: "center", fontSize: "5rem" }}>
+                Cart Empty
+            </h2>
+        );
+
+    return (
+        <div>
+            {cart.map((product) => (
+                <div className="detail cart">
+                    <img src={product.images.url} alt="" />
+
+                    <div className="box-detail">
+                        <h2>{product.title}</h2>
+
+                        <h3>$ {product.price * product.quantity}</h3>
+                        <p>{product.description}</p>
+                        <p>{product.content}</p>
+
+                        <div className="amount">
+                            <button> - </button>
+                            <span>{product.quantity}</span>
+                            <button> + </button>
+                        </div>
+
+                        <div className="delete">X</div>
+                    </div>
+                </div>
+            ))}
+
+            <div className="total">
+                <h3>Total: $ 0</h3>
+                <Link to="#!">PayMent</Link>
+            </div>
+        </div>
+    );
 }
 
 export default Cart;
