715143de75c91ba67ad2c87631aa3237284f4158
Tan Gia Luong <tangia123@outlook.com.vn>
2023-04-07 15:36:02 +0700

    Fix

--- client_/src/api/ProductsAPI.js
+++ client_/src/api/ProductsAPI.js
@@ -12,7 +12,10 @@ function ProductsAPI() {
 
     useEffect(() => {
         const getProducts = async () => {
-            const res = await axios.get('/api/products?limit=${page * 9}&${category}&${sort}&title[regex]=${search}');
+            const res = await axios.get(
+                `/api/products?limit=${page *
+                    9}&${category}&${sort}&title[regex]=${search}`
+            );
             setProducts(res.data.products);
             setResult(res.data.result);
         };
