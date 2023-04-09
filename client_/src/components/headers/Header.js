89ba6688cd0aa447860c8bf1c352745c445db8e8
Tan Gia Luong <tangia123@outlook.com.vn>
2023-04-10 00:38:50 +0700

    Header fix layout

--- client_/src/components/headers/Header.js
+++ client_/src/components/headers/Header.js
@@ -20,16 +20,16 @@ function Header() {
 
         window.location.href = "/";
     };
-
+    console.log(state);
     const adminRouter = () => {
         return (
             <>
                 <li>
-                    <Link to="/create_product"> Create Product </Link>{" "}
-                </li>{" "}
+                    <Link to="/create_product">Create Product</Link>
+                </li>
                 <li>
-                    <Link to="/category"> Categories </Link>{" "}
-                </li>{" "}
+                    <Link to="/category">Categories</Link>
+                </li>
             </>
         );
     };
@@ -38,53 +38,66 @@ function Header() {
         return (
             <>
                 <li>
-                    <Link to="/history"> History </Link>{" "}
-                </li>{" "}
+                    <Link to="/history">History</Link>
+                </li>
                 <li>
                     <Link to="/" onClick={logoutUser}>
-                        Logout{" "}
-                    </Link>{" "}
-                </li>{" "}
+                        Logout
+                    </Link>
+                </li>
             </>
         );
     };
 
     return (
-        <header>
-            <div className="menu">
-                <img src={Menu} alt="" width="30" />
-            </div>
-            <div className="logo">
-                <h1>
-                    <Link to="/"> {isAdmin ? "Admin" : "Shop MP"} </Link>{" "}
-                </h1>{" "}
-            </div>
-            <ul>
-                <li>
-                    <Link to="/"> {isAdmin ? "Products" : "Shop"} </Link>{" "}
-                </li>
-                {isAdmin && adminRouter()}{" "}
-                {isLogged ? (
-                    loggedRouter()
-                ) : (
-                    <li>
-                        <Link to="/login"> Login~Register </Link>{" "}
-                    </li>
-                )}
-                <li>
-                    <img src={Close} alt="" width="30" className="menu" />
-                </li>{" "}
-            </ul>
-            {isAdmin ? (
-                ""
-            ) : (
-                <div className="cart-icon">
-                    <span> {cart.length} </span>{" "}
-                    <Link to="/cart">
-                        <img src={Cart} alt="" width="30" />
-                    </Link>{" "}
+        <header class="p-3 bg-dark text-white">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <div className="menu">
+                        <img src={Menu} alt="" width="30" />
+                    </div>
+                    <div className="logo">
+                        <h1>
+                            <Link to="/">{isAdmin ? "Admin" : "Shop MP"}</Link>
+                        </h1>
+                    </div>
+
+                    <ul>
+                        <li>
+                            <Link to="/">{isAdmin ? "Products" : "Shop"}</Link>
+                        </li>
+
+                        {isAdmin && adminRouter()}
+                        {isLogged ? (
+                            loggedRouter()
+                        ) : (
+                            <li>
+                                <Link to="/login">Login ~ Register</Link>
+                            </li>
+                        )}
+
+                        <li>
+                            <img
+                                src={Close}
+                                alt=""
+                                width="30"
+                                className="menu"
+                            />
+                        </li>
+                    </ul>
+
+                    {isAdmin ? (
+                        ""
+                    ) : (
+                        <div className="cart-icon">
+                            <Link to="/cart">
+                                <span>{cart.length}</span>
+                                <img src={Cart} alt="" width="30" />
+                            </Link>
+                        </div>
+                    )}
                 </div>
-            )}{" "}
+            </div>
         </header>
     );
 }
